0a416ff6cf4e1d5b4ff67c40b479ac169972efa2
Yauhen Yemelyanau <yauhen_yemelyanau@epam.com>
2015-06-24 21:02:00 -0700

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gradientsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gradientsApp
+ */
+angular.module('gradientsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
