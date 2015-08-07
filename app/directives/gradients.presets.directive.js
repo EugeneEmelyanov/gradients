483e6dbe9c8b1e63c8ea35f7bb4cbf985463d2e1
unknown <Yauhen_Yemelyanau@EPUSPRIW0374.princeton.epam.com>
2015-08-07 15:36:48 -0700

    presets. in-progress

---
+++ app/directives/gradients.presets.directive.js
@@ -0,0 +1,13 @@
+(function() {
+
+  var directives = angular.module("directives");
+
+  directives.directive("gradients.presets", function() {
+    return {
+      templateUrl: "/directives/gradient.presets.template.html",
+      link: function(scope, elem, attr) {
+        console.log('link: ');
+      }
+    }
+  });
+})();
--- app/scripts/controllers/gradient.controller.js
+++ app/scripts/controllers/gradient.controller.js
@@ -37,7 +37,6 @@
 
             function validateString() {
               $scope.styleString = calculateStyles($scope.gradient, $scope.data, $scope.gradientStops);
-              $scope.$digest();
             }
 
             $scope.addStop = function(event) {
@@ -56,6 +55,40 @@
               $scope.gradientStops[index].color = color;
               validateString();
             }
+
+            $scope.presetItems = [
+              {gradient: {
+                gradientStyle: "Linear",
+                gradientDirection: "to right",
+                stops: [
+                  {location: 0,
+                   color: "#008080"},
+                  {location: 25,
+                   color: "#FFFFFF"},
+                  {location: 50,
+                   color: "#05C1FF"},
+                  {location: 75,
+                   color: "#FFFFFF"},
+                  {location:100,
+                   color: "#005757"}
+                ]
+              }},
+              { gradient: {
+                  gradientStyle: "Linear",
+                    gradientDirection: "to right",
+                    stops: [
+                    {location: 0,
+                      color: "#FF0000"},
+                    {location:100,
+                      color: "#000000"}
+                  ]
+              }},
+            ];
+
+           for ( var i = 0; i < $scope.presetItems.length; i ++) {
+             var item = $scope.presetItems[i];
+             item.styleString = calculateStyles(item, $scope.data, item.gradient.stops);
+           }
 
             $scope.gradientStops = [{
               location: 0,
