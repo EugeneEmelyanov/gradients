1a2586e0c62424c65183b690df7c88cee4bc4ecb
eugene <eugene@eugenes-MacBook-Air.local>
2015-08-25 01:03:16 -0700

    Serve integration

--- app/services/api.endpoints.constants.js
+++ app/services/api.endpoints.constants.js
@@ -3,10 +3,10 @@
 
     module.factory("API", function(config) {
         return {
-            "GRADIENT_STYLES": config.baseUrl + "/resources/gradientStyles.json",
+            "GRADIENT_STYLES": "http://eugenecanhelp-app1ica.rhcloud.com/api/v1/gradientstyles",
             "LINEAR_GRADIENT_DIRECTIONS": config.baseUrl + "/resources/linearGradientDirections.json",
             "RADIAL_GRADIENT_POSITIONS": config.baseUrl + "/resources/radialGradientPositions.json",
-            "RADIAL_GRADIENT_SIZES": config.baseUrl + "/resources/radialGradientSizes.json",
+            "RADIAL_GRADIENT_SIZES": "http://eugenecanhelp-app1ica.rhcloud.com/api/v1/radialgradientsizes",
             "GRADIENT_PRESETS": config.baseUrl + "/resources/presets.json",
             "CSS_TEMPLATE": config.baseUrl + "resources/csstemplate"
         }
